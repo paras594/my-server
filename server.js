292594602a83a60e8ef983bbd94306f99a82bcf8
paras594 <parasarora594@gmail.com>
2022-01-22 13:30:36 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.json({
+		port: process.env.PORT,
+		success: true,
+		message: "This is mahh server boi",
+	});
+});
+
+app.listen(5000 || process.env.PORT, () => {
+	console.log(`Server started on port: 5000`);
+});
